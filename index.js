7812be135dcf0c312913a60a3e885a795ebc1b09
MMendes24 <mmbusiness1@protonmail.com>
2020-06-18 14:31:31 -0400

    Task four completed

--- index.js
+++ index.js
@@ -112,9 +112,25 @@ drive(distance){
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
-class Lambdasian {
 
+class Lambdasian {
+  constructor(attributes){
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}.`
+  }
 }
+
+const mars = new Lambdasian({
+  name: 'Mars',
+  age: 25,
+  location: 'Biloxi'
+});
+
+console.log(mars.speak())
 
 /*
   TASK 4
@@ -130,8 +146,20 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
 
+class Instructor extends Lambdasian{
+  constructor(attributes){
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}.`
+  }
+  grade(subject, student){
+    return `${student.name} received a perfect score on ${subject}.`
+  }
 }
 
 /*
